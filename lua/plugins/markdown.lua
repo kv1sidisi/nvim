83e646fbc9ce25b434a08c475e296253a1048dfb
Vadim Lyapunov <lyapunovvadim5002@gmail.com>
2025-09-28 13:24:26 +0300

    wrong md viewer deleted

--- lua/plugins/markdown.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ellisonleao/glow.nvim",
-  config = function()
-    require("glow").setup({})
-    vim.keymap.set("n", "<leader>mp", "<cmd>Glow<CR>", { desc = "[M]arkdown [P]review" })
-  end,
-}
